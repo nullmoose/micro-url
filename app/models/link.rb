1b469d764505b30cd4bcddefbeffbd75cb299619
Cody Burchell <cody.burchell@gmail.com>
2021-04-07 21:47:30 -0500

    Add slug generation to links
    * Slugs are generated before validation on create
    * Validate the presence of slugs

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,14 @@
 class Link < ApplicationRecord
   validates :original_url, presence: true, format: URI::regexp(%w[http https])
+  validates :short_slug, presence: true
+  validates :admin_slug, presence: true
+
+  before_validation :generate_slugs, on: :create
+
+  private
+
+  def generate_slugs
+    self.short_slug = SecureRandom.uuid[0..10]
+    self.admin_slug = SecureRandom.uuid[0..10]
+  end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe Link do
   it "should require original_url to be a url" do
     expect(Link.new(original_url: "not a url")).to_not be_valid
   end
+
+  describe "before_validation" do
+    it "should generate an admin slug on create" do
+      expect(Link.create(original_url: "https://www.test.com").admin_slug).to_not be_nil
+    end
+
+    it "should generate a short slug on create" do
+      expect(Link.create(original_url: "https://www.test.com").short_slug).to_not be_nil
+    end
+
+    it "should not change slugs on update" do
+      link = Link.create(original_url: "https://www.test.com")
+      expect {
+        link.update(click_counter: 1)
+      }.to_not change(link, :admin_slug)
+    end
+  end
 end
